14f1490de08d3bfa027ac1b8d4867b54a17a4de0
William Mendonça <william@tecmasternet.com.br>
2021-06-11 19:19:01 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+
+import { SearchBar } from '../../components/SearchBar';
+import { LoginDataItem } from '../../components/LoginDataItem';
+
+import {
+  Container,
+  LoginList,
+  EmptyListContainer,
+  EmptyListMessage
+} from './styles';
+
+interface LoginDataProps {
+  id: string;
+  title: string;
+  email: string;
+  password: string;
+};
+
+type LoginListDataProps = LoginDataProps[];
+
+export function Home() {
+  const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
+  const [data, setData] = useState<LoginListDataProps>([]);
+  const dataKey = '@passmanager:logins';
+
+  async function loadData() {
+    try {
+      const response = await AsyncStorage.getItem(dataKey);
+
+      if(!response) return;
+
+      const currentData = response ? JSON.parse(response) : [];
+    
+      setSearchListData(currentData);
+      setData(currentData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  useFocusEffect(useCallback(() => {
+    loadData();
+  }, []));
+
+  function handleFilterLoginData(search: string) {
+    if(search){
+      const dataFilttered = data.filter(item => item.title.includes(search));
+
+      if(dataFilttered) {
+        setSearchListData(dataFilttered);
+      }
+    } else {
+      setSearchListData(data);
+    }
+  }
+
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Pesquise pelo nome do serviço"
+        onChangeText={(value) => handleFilterLoginData(value)}
+      />
+
+      <LoginList
+        keyExtractor={(item) => item.id}
+        data={searchListData}
+        ListEmptyComponent={(
+          <EmptyListContainer>
+            <EmptyListMessage>Nenhum item a ser mostrado</EmptyListMessage>
+          </EmptyListContainer>
+        )}
+        renderItem={({ item: loginData }) => {
+          return <LoginDataItem
+            title={loginData.title}
+            email={loginData.email}
+            password={loginData.password}
+          />
+        }}
+      />
+    </Container>
+  )
+}
