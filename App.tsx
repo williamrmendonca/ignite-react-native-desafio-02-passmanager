4ff44bf4635598ceae6ca37d2253bc342cbf1d4a
William Mendonça <william@tecmasternet.com.br>
2021-06-12 22:35:52 -0300

    change themes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import { NavigationContainer } from '@react-navigation/native';
